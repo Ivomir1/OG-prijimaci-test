bf96d2ebe85280be57c190747c7c605c21f1d1bb
Ivo Burian <burian@cinovahora.cz>
2024-08-13 14:30:54 +0200

    Přidávám jen tak pro radost autorizování uživatelů hotovson

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
